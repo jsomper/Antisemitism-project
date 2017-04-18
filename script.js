5c3d0cceab47f20ddc969e41a7cb747a18a76190
jsomper <jamessomper6@gmail.com>
2017-04-18 15:28:41 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.myfunction = function () {
   var myWindow = window.open("",  "", "width=200, height=100");
-  var dd = document.getElementById("dropdown");
+  var dd = document.getElementById("button");
   myWindow.document.write(dd.options[dd.selectedIndex].innerHTML);
 }
+
